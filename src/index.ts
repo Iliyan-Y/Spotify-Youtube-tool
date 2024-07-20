2e01cf901f731d77887f993479939b6d280b68ef
Iliyan-Y <maximizebg@gmail.com>
2024-07-20 14:17:38 +0100

    Wip get access to spotify

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,35 @@
 import "dotenv/config";
 import { env } from "process";
+import axios from "axios";
 
-const testValue = env.TEST_VALUE;
-export const main = (): string => testValue;
+const spotifyClientId = env.SPOTIFY_CLIENT_ID;
+const spotifyClientSecret = env.SPOTIFY_CLIENT_SECRET;
 
-console.log(main());
+const getSpotifyAccessToken = async (): Promise<string> => {
+	if (!spotifyClientId || !spotifyClientSecret)
+		throw new Error("SPOTIFY_CLIENT_ID and SPOTIFY_CLIENT_SECRET are required");
+
+	const url = "https://accounts.spotify.com/api/token";
+
+	const data = await axios.post(
+		url,
+		{
+			grant_type: "client_credentials",
+			client_id: spotifyClientId,
+			client_secret: spotifyClientSecret,
+		},
+		{
+			headers: {
+				"Content-Type": "application/x-www-form-urlencoded",
+			},
+		}
+	);
+
+	return data.data.access_token;
+};
+
+export const main = async (): Promise<void> => {
+	const token = await getSpotifyAccessToken();
+};
+
+main();
